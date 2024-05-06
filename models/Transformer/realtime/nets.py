e5e0bab115594db5e6530c42dd19bc8bae0160ba
meiyoufeng116 <meiyoufeng116@gmail.com>
2024-05-06 11:52:40 +0800

    update transformer

---
+++ models/Transformer/realtime/nets.py
@@ -0,0 +1,57 @@
+import torch
+import torch.nn as nn
+import math
+import torch.nn.functional as F
+
+
+class Transformer(nn.Module):
+
+    def __init__(self,input_dim,dropout=0.1):
+        super(Transformer, self).__init__()
+        self.embedding1 = nn.Sequential(
+            nn.Linear(input_dim, 128),
+        )
+        self.dropout = dropout
+        self.encoderlayer=nn.TransformerEncoderLayer(d_model=128, nhead=8,dim_feedforward=1024,dropout=self.dropout)
+        self.encoder=nn.TransformerEncoder(self.encoderlayer, num_layers=6)
+        
+        self.conv = nn.Conv1d(2500, 1, 1)
+        self.gelu = nn.GELU()
+        self.pos_encoder = PositionalEncoding(128, dropout=self.dropout, max_len=3750)
+        self.classifier = nn.Sequential(
+            nn.Conv1d(128, 1, 1)
+        )
+
+    def forward(self, signal):  # x (batch, time_step, input_size)
+
+        input = self.embedding1(signal.permute(0, 2, 1))
+        input = self.pos_encoder(input)
+
+        encoder_output = self.encoder(input,mask=None)
+
+        output = self.conv(encoder_output)
+        output = torch.transpose(output, 1, 2)
+        output = self.gelu(output)
+
+        output = self.classifier(output).squeeze(2)
+
+        return encoder_output, output
+
+class PositionalEncoding(nn.Module):
+    def __init__(self, d_model, dropout=0.1, max_len=1250):
+        super(PositionalEncoding, self).__init__()
+        self.dropout = nn.Dropout(p=dropout)
+
+        pe = torch.zeros(max_len, d_model)
+        position = torch.arange(0, max_len, dtype=torch.float).unsqueeze(1)
+        div_term = torch.exp(torch.arange(0, d_model, 2).float() * -(math.log(10000.0) / d_model))
+        pe[:, 0::2] = torch.sin(position * div_term)
+        pe[:, 1::2] = torch.cos(position * div_term)
+        pe = pe.unsqueeze(0).transpose(0, 1)
+        self.register_buffer('pe', pe)
+
+    def forward(self, x):
+        x = x + self.pe[:x.size(0), :]
+        return self.dropout(x)
+    
+
---
+++ models/Transformer/retrospective/tools.py
@@ -0,0 +1,161 @@
+import logging
+import numpy as np
+import torch
+import torch.nn.functional as F
+from torch.utils.data import Dataset
+
+
+def get_logger(
+    logpath, filepath, package_files=[], displaying=True, saving=True, debug=False
+):
+    logger = logging.getLogger()
+    if debug:
+        level = logging.DEBUG
+    else:
+        level = logging.INFO
+    logger.setLevel(level)
+    if saving:
+        info_file_handler = logging.FileHandler(logpath, mode="w")
+        info_file_handler.setLevel(level)
+        logger.addHandler(info_file_handler)
+    if displaying:
+        console_handler = logging.StreamHandler()
+        console_handler.setLevel(level)
+        logger.addHandler(console_handler)
+
+    logger.info(filepath)
+
+    for f in package_files:
+        logger.info(f)
+        with open(f, "r") as package_f:
+            logger.info(package_f.read())
+
+    return logger
+
+
+class Dataset_train(Dataset):
+    # 'Characterizes a dataset for PyTorch'
+    def __init__(self, signal_train, y_train):
+        # signal
+        self.strain = signal_train
+        # groundtruth
+        self.ytrain = y_train
+
+    def __len__(self):
+        # 'Denotes the total number of samples'
+        return len(self.ytrain)
+
+    def __getitem__(self, index):
+        # 'Generates one sample of data'
+        # Select sample
+        return self.strain[index], self.ytrain[index]
+
+
+def train_model(batch, model, loss_ce, device, weight):
+    signal_train, y_train = batch
+    # use the last 10s signal as model input
+    signal_train = signal_train[:, :, 72500:76250].to(device)
+    y_train = y_train.to(device)
+    # model prediction, feature of alarm signal, feature of randomly selected signal
+    _,Y_train_prediction = model(signal_train)
+    # calculate loss
+    loss = loss_ce(Y_train_prediction, y_train)
+    return loss, Y_train_prediction, y_train
+
+
+def eval_model(batch, model, loss_ce, device):
+    signal_train, y_train = batch
+    length = 3750
+    # alarm signa
+    signal_train = signal_train[:, :, 72500:76250].to(device)
+
+    y_train = y_train.to(device)
+
+    # prediction
+    _,Y_train_prediction = model(signal_train)
+
+    loss = loss_ce(Y_train_prediction, y_train)
+
+    return loss, Y_train_prediction, y_train
+
+
+def evaluation(Y_eval_prediction, y_test, TP, FP, TN, FN):
+    # set 0 if false alarm else 1
+    pre = (Y_eval_prediction >= 0).int()
+    # pair: (prediction, groundtruth)
+    for i, j in zip(pre, y_test):
+        if i.item() == 1 and j.item() == 1:
+            TP += 1
+        if i.item() == 1 and j.item() == 0:
+            FP += 1
+        if i.item() == 0 and j.item() == 0:
+            TN += 1
+        if i.item() == 0 and j.item() == 1:
+            FN += 1
+    return TP, FP, TN, FN
+
+
+def evaluate_rule_based(rule_based_results, y_test):
+    TP = FP = TN = FN = 0
+    # pair: (prediction, groundtruth)
+    for i, j in zip(rule_based_results, y_test):
+        if i.item() == 1 and j.item() == 1:
+            TP += 1
+        if i.item() == 1 and j.item() == 0:
+            FP += 1
+        if i.item() == 0 and j.item() == 0:
+            TN += 1
+        if i.item() == 0 and j.item() == 1:
+            FN += 1
+    return (
+        100 * TP / (TP + FN),
+        100 * TN / (TN + FP),
+        100 * (TP + TN) / (TP + TN + FP + 5 * FN),
+        100 * (TP + TN) / (TP + TN + FP + FN),
+    )
+
+
+def evaluation_test(Y_eval_prediction, y_test, types_TP, types_FP, types_TN, types_FN):
+    pre = (Y_eval_prediction >= 0).int()
+    for i, j in zip(pre, y_test):
+        if i.item() == 1 and j.item() == 1:
+            types_TP += 1
+        if i.item() == 1 and j.item() == 0:
+            types_FP += 1
+        if i.item() == 0 and j.item() == 0:
+            types_TN += 1
+        if i.item() == 0 and j.item() == 1:
+            types_FN += 1
+    return types_TP, types_FP, types_TN, types_FN
+
+
+def evaluate_raise_threshold(
+    prediction, groundtruth, types_TP, types_FP, types_TN, types_FN, threshold
+):
+    prediction = torch.sigmoid(prediction)
+
+    pre = 1 if prediction >= threshold else 0
+    # identify if a sample is TP, FP, TN, or FN
+    if pre == 1 and groundtruth == 1:
+        types_TP += 1
+    elif pre == 1 and groundtruth == 0:
+        types_FP += 1
+    elif pre == 0 and groundtruth == 1:
+        types_FN += 1
+    elif pre == 0 and groundtruth == 0:
+        types_TN += 1
+
+    return types_TP, types_FP, types_TN, types_FN
+
+
+def eval(batch, model, device):
+    signal_train, y_train = batch
+    length = 3750
+
+    signal_train = signal_train[:, :, 72500:76250].to(device)
+
+    y_train = y_train.to(device)
+
+    Y_train_prediction = model(signal_train)
+
+    return Y_train_prediction, y_train
